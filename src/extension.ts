4861221fba4dd4b52ee0aeeb557f937461ce7a0b
guza <guza@duck.com>
2025-04-13 10:13:43 +0800

    feat: add close all deletes files support

--- src/extension.ts
+++ src/extension.ts
@@ -2,17 +2,18 @@ import * as vscode from "vscode";
 import { INTERVAL_IN_MINUTES, lg } from "./common";
 import { getSettingValue, updateSettingValue } from "./settings";
 import {
-	archiveTabs,
-	clearArchivedTabs,
-	closeAllDiffTabs,
-	createTabTimeCounters,
-	incrementTabTimeCounter,
-	listArchivedTabs,
-	loadArchivedTabs,
-	removeTabTimeCounter,
-	resetTabTimeCounter,
-	storeArchivedTabs,
-	storeTabTimeCounters,
+    archiveTabs,
+    clearArchivedTabs,
+    closeAllDeletedFiles,
+    closeAllDiffTabs,
+    createTabTimeCounters,
+    incrementTabTimeCounter,
+    listArchivedTabs,
+    loadArchivedTabs,
+    removeTabTimeCounter,
+    resetTabTimeCounter,
+    storeArchivedTabs,
+    storeTabTimeCounters,
 } from "./tabarchive";
 
 let interval: ReturnType<typeof setInterval>;
@@ -139,6 +140,13 @@ const registerCommands = (context: vscode.ExtensionContext) => {
 		vscode.commands.registerCommand(
 			"tabarchive.closeAllDiffTabs",
 			() => closeAllDiffTabs(),
+		),
+	);
+
+	context.subscriptions.push(
+		vscode.commands.registerCommand(
+			"tabarchive.closeAllDeletedFiles",
+			() => closeAllDeletedFiles(),
 		),
 	);
 };
